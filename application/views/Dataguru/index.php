7ee83a4fe81209f74765db24b78d747ebc88af9a
zidanR <64406817+zidanR@users.noreply.github.com>
2021-09-13 10:57:22 +0700

    Hapus Edit guru

--- application/views/Dataguru/index.php
+++ application/views/Dataguru/index.php
@@ -28,7 +28,8 @@
                                 <th>No</th>
                                 <th>Nama</th>
                                 <th>No Induk</th>
-                                <th>Jenis Kelamin</th>                                
+                                <th>Jenis Kelamin</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -41,13 +42,47 @@
                                     <td><?= $no++ ?></td>
                                     <td><?= $s['nama_guru'] ?></td>
                                     <td><?= $s['no_induk_guru'] ?></td>
-                                    <td><?= $s['jenis_kelamin'] ?></td>                                    
+                                    <td><?= $s['jenis_kelamin'] ?></td>
+                                    <td>
+                                        <a class="btn btn-warning" href="<?= base_url('dataguru/edit/') . $s['id'] ?>">Edit</a>
+                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter" onclick="klik(<?= $s['id'] ?>)"> Hapus</button>
+                                    </td>
                                 </tr>
+                                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLongTitle">Hapus Data </h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Yakin ingin menghapus data ini ?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <form action="dataguru/delete/id_hapus" method="POST">
+                                                    <!-- <input type="text" id="id_hapus"> -->
+                                                    <button type="submit" class="btn btn-danger" value="delete">Hapus</button>
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                                </form>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <script>
+                                    function klik(data) {
+                                        var a = data
+                                        var b = document.getElementById('id_hapus').value = a
+                                    }
+                                </script>
                             <?php endforeach ?>
+                            <!-- Modal -->
                         </tbody>
                         <tfoot>
                             <tr>
-                            <!-- <th>No</th>
+                                <!-- <th>No</th>
                                 <th>Nama</th>
                                 <th>No Induk</th>
                                 <th>Jenis Kelamin</th>
@@ -57,8 +92,6 @@
                 </div>
             </div>
         </div>
-
-
     </div>
     <!-- /.content -->
 </div>
