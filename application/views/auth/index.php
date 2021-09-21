df92acb98528120bc9ff6b64f4fb78dff7c53324
zidanR <64406817+zidanR@users.noreply.github.com>
2021-09-21 10:35:24 +0700

    Update index.php

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Niche Admin - Powerful Bootstrap 4 Dashboard and Admin Template</title>
+  <title>Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
